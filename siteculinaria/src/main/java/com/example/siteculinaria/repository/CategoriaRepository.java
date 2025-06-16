3113d97690e5e38fbdf0e149fa7a9bd25c4db232
FehMell <139819379+FehMell@users.noreply.github.com>
2025-06-15 23:23:12 -0300

    Delete siteculinaria directory

--- siteculinaria/src/main/java/com/example/siteculinaria/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.siteculinaria.repository;
-
-import com.example.siteculinaria.models.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
--- siteculinaria/src/main/java/com/example/siteculinaria/repository/ReceitaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.siteculinaria.repository;
-
-import com.example.siteculinaria.models.Receita;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReceitaRepository extends JpaRepository<Receita, Long> {
-}
